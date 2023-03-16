026445c9b1ea44a527757960ccdded71be95713e
Yuzhuo Kang <yuzhuok@seas.upenn.edu>
2023-03-16 03:21:11 -0400

    push hw4

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -4,22 +4,26 @@ import torch.nn.functional as F
 
 
 class Model(torch.nn.Module):
+    """_summary_"""
+
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
         self.conv1 = nn.Conv2d(
             in_channels=num_channels, out_channels=16, kernel_size=3, padding=0
         )
         self.conv2 = nn.Conv2d(
-            in_channels=16, out_channels=32, kernel_size=3, padding=0
+            in_channels=16, out_channels=24, kernel_size=3, padding=0
         )
         self.bn1 = nn.BatchNorm2d(16)
-        self.bn2 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(24)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)  # (27-2)/2 + 1 = 13
-        self.fc = nn.Linear(32 * 6 * 6, num_classes)
+        self.fc = nn.Linear(24 * 6 * 6, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """_summary_"""
+
         x = self.pool(F.leaky_relu(self.bn1(self.conv1(x))))
         x = self.pool(F.leaky_relu(self.bn2(self.conv2(x))))
-        x = x.view(-1, 32 * 6 * 6)
+        x = x.view(-1, 24 * 6 * 6)
         x = self.fc(x)
         return x
