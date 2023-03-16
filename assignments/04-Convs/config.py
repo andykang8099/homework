4e06f530cc157978509230fc51a92354215a2dff
Yuzhuo Kang <yuzhuok@seas.upenn.edu>
2023-03-16 03:33:51 -0400

    push hw4

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -2,8 +2,7 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-import torchvision
-from torchvision.transforms import Compose, ToTensor
+from torchvision.transforms import Compose, ToTensor, Normalize
 
 
 class CONFIG:
@@ -14,4 +13,6 @@ class CONFIG:
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(model.parameters(), lr=1e-3)
 
-    transforms = Compose([ToTensor()])
+    transforms = Compose(
+        [ToTensor(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]
+    )
--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -9,21 +9,19 @@ class Model(torch.nn.Module):
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
         self.conv1 = nn.Conv2d(
-            in_channels=num_channels, out_channels=16, kernel_size=3, padding=0
+            in_channels=num_channels, out_channels=8, kernel_size=3, padding=0
         )
-        self.conv2 = nn.Conv2d(
-            in_channels=16, out_channels=32, kernel_size=3, padding=0
-        )
-        self.bn1 = nn.BatchNorm2d(16)
-        self.bn2 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, padding=0)
+        self.bn1 = nn.BatchNorm2d(8)
+        self.bn2 = nn.BatchNorm2d(16)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)  # (27-2)/2 + 1 = 13
-        self.fc = nn.Linear(32 * 6 * 6, num_classes)
+        self.fc = nn.Linear(16 * 6 * 6, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """_summary_"""
 
         x = self.pool(F.leaky_relu(self.bn1(self.conv1(x))))
         x = self.pool(F.leaky_relu(self.bn2(self.conv2(x))))
-        x = x.view(-1, 32 * 6 * 6)
+        x = x.view(-1, 16 * 6 * 6)
         x = self.fc(x)
         return x
