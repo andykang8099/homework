5809b46909f764e2012f0d89fa3ab4f6828e2d97
Yuzhuo Kang <yuzhuok@seas.upenn.edu>
2023-03-18 02:24:22 -0400

    push hw4

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -6,12 +6,12 @@ from torchvision.transforms import Compose, ToTensor, Normalize
 
 
 class CONFIG:
-    batch_size = 70
+    batch_size = 150
     num_epochs = 6
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
-    ] = lambda model: torch.optim.Adam(model.parameters(), lr=3e-3)
+    ] = lambda model: torch.optim.AdamW(model.parameters(), lr=3e-3)
 
     transforms = Compose(
         [ToTensor(), Normalize(mean=[0.5, 0.5, 0.5], std=[0.1, 0.1, 0.1])]
--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -10,27 +10,26 @@ class Model(torch.nn.Module):
         super(Model, self).__init__()
         self.conv1 = nn.Conv2d(
             in_channels=num_channels,
-            out_channels=10,
+            out_channels=8,
             kernel_size=2,
             stride=2,
             padding=0,
         )
         # self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, padding=0)
-        self.bn1 = nn.BatchNorm2d(10)
+        self.bn1 = nn.BatchNorm2d(8)
 
-        self.bn2 = nn.BatchNorm1d(32)
+        self.bn2 = nn.BatchNorm1d(36)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)  # (27-2)/2 + 1 = 13
         # self.fc1 = nn.Linear(12 * 16 * 16, 256)
-        self.fc2 = nn.Linear(10 * 8 * 8, 32)
-
-        self.fc1 = nn.Linear(32, num_classes)
+        self.fc2 = nn.Linear(8 * 8 * 8, 36)
+        self.fc1 = nn.Linear(36, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """_summary_"""
 
         x = self.pool(F.relu(self.bn1(self.conv1(x))))
         # x = F.relu(self.bn2(self.conv2(x))))
-        x = x.view(-1, 10 * 8 * 8)
+        x = x.view(-1, 8 * 8 * 8)
         x = F.relu(self.bn2(self.fc2(x)))
         x = self.fc1(x)
         return x
