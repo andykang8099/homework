b88aad78c960a5c940ec59bb49de129b3361482e
Yuzhuo Kang <yuzhuok@seas.upenn.edu>
2023-03-18 02:03:28 -0400

    push hw4

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -11,7 +11,7 @@ class Model(torch.nn.Module):
         self.conv1 = nn.Conv2d(
             in_channels=num_channels,
             out_channels=10,
-            kernel_size=3,
+            kernel_size=2,
             stride=2,
             padding=0,
         )
@@ -21,7 +21,7 @@ class Model(torch.nn.Module):
         self.bn2 = nn.BatchNorm1d(32)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)  # (27-2)/2 + 1 = 13
         # self.fc1 = nn.Linear(12 * 16 * 16, 256)
-        self.fc2 = nn.Linear(10 * 7 * 7, 32)
+        self.fc2 = nn.Linear(10 * 8 * 8, 32)
 
         self.fc1 = nn.Linear(32, num_classes)
 
@@ -30,7 +30,7 @@ class Model(torch.nn.Module):
 
         x = self.pool(F.relu(self.bn1(self.conv1(x))))
         # x = F.relu(self.bn2(self.conv2(x))))
-        x = x.view(-1, 10 * 7 * 7)
+        x = x.view(-1, 10 * 8 * 8)
         x = F.relu(self.bn2(self.fc2(x)))
         x = self.fc1(x)
         return x
