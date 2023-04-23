dc315b28975535de43d3a9511b3474db6a9fd512
Yuzhuo Kang <yuzhuok@seas.upenn.edu>
2023-04-22 21:06:11 -0400

    push hw5

--- assignments/05-RL/customagent.py
+++ assignments/05-RL/customagent.py
@@ -32,7 +32,7 @@ class Agent:
     ):
         self.action_space = action_space
         self.observation_space = observation_space
-        self.memory = []  # set the memory
+        self.memory = []
         self.batch_size = 512
         self.epsilon = 1.0
         self.epsilon_decay = 0.995
